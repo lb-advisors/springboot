c312dd4d7d2f6b2f6dcb941f86108424b6f924d7
Olivier Leblond <oleblond@psi-it.com>
2024-06-27 19:23:14 -0400

    Renamed built

--- src/main/java/com/lbadvisors/pffc/info/AppInfoController.java
+++ src/main/java/com/lbadvisors/pffc/info/AppInfoController.java
@@ -23,30 +23,23 @@ public class AppInfoController {
     @GetMapping(value = "")
     public ResponseEntity<AppInfo> getAppInfo() {
 
-        /*
-         * Instant instant = Instant.parse(env.getProperty("BUILT_TIME"));
-         * 
-         * // Convert Instant to ZonedDateTime with UTC timezone
-         * ZonedDateTime utcDateTime = ZonedDateTime.ofInstant(instant,
-         * ZoneId.of("UTC"));
-         * 
-         * // Convert UTC ZonedDateTime to Eastern Standard Time (EST) or Eastern
-         * Daylight
-         * // Time (EDT)
-         * ZonedDateTime estDateTime =
-         * utcDateTime.withZoneSameInstant(ZoneId.of("America/New_York"));
-         * 
-         * // Define a formatter for the output format
-         * DateTimeFormatter formatter =
-         * DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-         * 
-         * // Format EST ZonedDateTime to human-readable string
-         * String formattedDateTime = estDateTime.format(formatter) + " EST";
-         * 
-         */
+        Instant instant = Instant.parse(env.getProperty("BUILD_TIME"));
+
+        // Convert Instant to ZonedDateTime with UTC timezone
+        ZonedDateTime utcDateTime = ZonedDateTime.ofInstant(instant, ZoneId.of("UTC"));
+
+        // Convert UTC ZonedDateTime to Eastern Standard Time (EST) or Eastern Daylight
+        // Time (EDT)
+        ZonedDateTime estDateTime = utcDateTime.withZoneSameInstant(ZoneId.of("America/New_York"));
+
+        // Define a formatter for the output format
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // Format EST ZonedDateTime to human-readable string
+        String formattedDateTime = estDateTime.format(formatter) + " EST";
 
         AppInfo appInfo = new AppInfo();
-        appInfo.builtTime = "test" + env.getProperty("BUILT_TIME");// formattedDateTime;
+        appInfo.builtTime = formattedDateTime;
         appInfo.commitMessage = env.getProperty("COMMIT_MESSAGE");
         appInfo.commitHash = env.getProperty("COMMIT_HASH");
 
