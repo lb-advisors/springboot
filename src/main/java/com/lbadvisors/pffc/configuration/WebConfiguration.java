84677098346c7c1cd7a115126fd966a3bcfda2a1
Olivier Leblond <oleblond@psi-it.com>
2024-06-28 22:20:37 -0400

    Cors

--- src/main/java/com/lbadvisors/pffc/configuration/WebConfiguration.java
+++ src/main/java/com/lbadvisors/pffc/configuration/WebConfiguration.java
@@ -10,6 +10,6 @@ public class WebConfiguration implements WebMvcConfigurer {
     @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
+        registry.addMapping("/**"); // .allowedMethods("*");
     }
 }
--- src/main/java/com/lbadvisors/pffc/delivery_stops/DeliveryStopController.java
+++ src/main/java/com/lbadvisors/pffc/delivery_stops/DeliveryStopController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lbadvisors.pffc.exception.ResourceNotFoundException;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/delivery-stops")
 public class DeliveryStopController {
 
