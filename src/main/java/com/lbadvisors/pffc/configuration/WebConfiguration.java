da55fa4ae16aed0a3e4886961178a2cce62ae8ee
Olivier Leblond <oleblond@psi-it.com>
2024-06-28 22:29:06 -0400

    Allow patch

--- src/main/java/com/lbadvisors/pffc/configuration/WebConfiguration.java
+++ src/main/java/com/lbadvisors/pffc/configuration/WebConfiguration.java
@@ -10,6 +10,8 @@ public class WebConfiguration implements WebMvcConfigurer {
     @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**"); // .allowedMethods("*");
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Allow specific HTTP
+                                                                                           // methods
+                .allowedHeaders("*"); // Allow all headers
     }
 }
