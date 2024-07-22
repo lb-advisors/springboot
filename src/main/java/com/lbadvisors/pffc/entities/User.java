b8ec5bbe7ed79537c4339301a749372a45ba3c07
Olivier Leblond <oleblond@psi-it.com>
2024-07-22 09:45:13 -0400

    Updated user table

--- src/main/java/com/lbadvisors/pffc/entities/User.java
+++ src/main/java/com/lbadvisors/pffc/entities/User.java
@@ -1,6 +1,12 @@
 package com.lbadvisors.pffc.entities;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -36,6 +42,30 @@ public class User {
 
     @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
+    private boolean isEnabled;
+
+    @Column(nullable = false)
+    private boolean isLocked;
+
+    private LocalDateTime lastLoginAt;
+    private Long loginCount;
+    private Long failedAttemptsCount;
+
+    @CreatedBy
+    private String createdBy;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedBy
+    private String lastUpdatedBy;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime lastUpdatedAt;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
