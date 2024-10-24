bb7d61caf09d8ab03728d4bea469fdad2c658856
Olivier Leblond <oleblond@psi-it.com>
2024-10-24 14:55:37 -0400

    Implemented 2am cutoff time using Pacific time

--- src/main/java/com/lbadvisors/pffc/validators/DynamicDateRangeValidator.java
+++ src/main/java/com/lbadvisors/pffc/validators/DynamicDateRangeValidator.java
@@ -1,7 +1,9 @@
 package com.lbadvisors.pffc.validators;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
@@ -11,14 +13,31 @@ public class DynamicDateRangeValidator implements ConstraintValidator<DynamicDat
     @Override
     public boolean isValid(LocalDate date, ConstraintValidatorContext context) {
         if (date == null) {
-            return true; // Use @NotNull if null values should be invalid
+            return true;
         }
-        try {
-            LocalDate startDate = LocalDate.now().plusDays(1);
-            LocalDate endDate = LocalDate.now().plusMonths(3);
-            return !date.isBefore(startDate) && !date.isAfter(endDate);
-        } catch (DateTimeParseException e) {
-            return false;
+
+        // Zone for Pacific Time
+        ZoneId pacificZone = ZoneId.of("America/Los_Angeles");
+
+        // Get the current Pacific time
+        ZonedDateTime nowPacific = ZonedDateTime.now(pacificZone);
+
+        // Get the next 2 AM in Pacific Time
+        ZonedDateTime next2amPacific = nowPacific.with(LocalTime.of(2, 0));
+        if (nowPacific.isAfter(next2amPacific)) {
+            // If it's already past 2 AM today, move to the next day 2 AM
+            next2amPacific = next2amPacific.plusDays(1);
         }
+
+        // Calculate 3 months from now at 2 AM
+        ZonedDateTime threeMonthsLaterPacific = next2amPacific.plusMonths(3);
+
+        // Convert the given LocalDate to ZonedDateTime at 2 AM in Pacific Time
+        ZonedDateTime givenDateTime = ZonedDateTime.of(date, LocalTime.of(2, 0), pacificZone);
+
+        // Check if the given date is between the next 2 AM and 3 months from now
+        boolean isInRange = !givenDateTime.isBefore(next2amPacific) && !givenDateTime.isAfter(threeMonthsLaterPacific);
+
+        return isInRange;
     }
 }
